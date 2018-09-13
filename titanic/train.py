9de078f13f4a38ef144a75009c9273f2206a1494
Mahmut Ozge Karakaya <mokarakaya@gmail.com>
2018-09-14 00:17:39 +0200

    add family data

--- titanic/train.py
+++ titanic/train.py
@@ -5,7 +5,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import linear_model as lm
 import numpy as np
-from titanic.util import encode_label, fill_na
+from titanic_util import fill_na, encode_label
 
 
 train_df = pd.read_csv('data/train.csv')
@@ -18,21 +18,29 @@ fill_na(train_df, test_df, 'Embarked', 'S')
 encode_label(train_df, test_df, 'Sex', 'SexE')
 encode_label(train_df, test_df, 'Embarked', 'EmbarkedE')
 
+train_df['Family_Size'] = train_df['SibSp'] + train_df['Parch'] + 1
+test_df['Family_Size'] = test_df['SibSp'] + test_df['Parch'] + 1
 
-feature_classes = ["Pclass", "SibSp", "Parch", "Fare", 'SexE', 'Age', 'EmbarkedE']
+train_df['Is_Alone'] = 0
+test_df['Is_Alone'] = 0
+train_df.loc[train_df['Family_Size'] == 1, 'Is_Alone'] = 1
+test_df.loc[test_df['Family_Size'] == 1, 'Is_Alone'] = 1
+
+
+feature_classes = ["Pclass", "SibSp", "Parch", "Fare", 'SexE', 'Age', 'EmbarkedE', 'Family_Size', 'Is_Alone']
 train_X = train_df[feature_classes]
 train_y = train_df["Survived"]
 test_X = test_df[feature_classes]
 
 
 X_train, X_test, y_train, y_test = train_test_split(train_X, train_y, test_size=0.33, random_state=42)
-# clf = MLPClassifier(solver='adam',
-#                     alpha=1e-5,
-#                     hidden_layer_sizes=(100, 8),
-#                     random_state=1,
-#                     activation='tanh',
-#                     learning_rate='adaptive')
-clf = RandomForestClassifier(max_depth= 2, random_state=0)
+clf = MLPClassifier(solver='adam',
+                    alpha=1e-5,
+                    hidden_layer_sizes=(100, 8),
+                    random_state=1,
+                    activation='tanh',
+                    learning_rate='adaptive')
+# clf = RandomForestClassifier(max_depth= 2, random_state=0)
 # clf = lm.LogisticRegression()
 
 
